25f0258bb02cdfe9337d910a3209d34f4c87b8c3
Кирилл Новиков <kirya.novikov.05@gmail.com>
2025-03-06 15:05:01 +0600

    Create initional commit

---
+++ src/test/java/com/example/backend/SkillSwap/SkillSwapApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.backend.SkillSwap;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SkillSwapApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
